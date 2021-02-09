0ef10cb2dfdcbd3a72b3fbf0088d7a4353cf4278
programming <mehdi.developer.sh@gmail.com>
2021-02-09 17:14:32 +0330

    beginning the project

---
+++ logger/src/main/java/com/sky/logger/Logger.java
@@ -0,0 +1,13 @@
+package com.sky.logger;
+
+import android.util.Log;
+
+public class Logger {
+    public static void i(String message) {
+        Log.i(Logger.class.getSimpleName(), message);
+    }
+
+    public static void e(String message) {
+        Log.e(Logger.class.getSimpleName(), message);
+    }
+}
